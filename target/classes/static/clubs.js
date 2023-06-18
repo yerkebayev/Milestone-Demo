bb0c10f62b491ca2f2ca71b7ddb69daaf9577185
yerkebayev <maratpyn@gmail.com>
2023-06-18 15:59:13 +0900

    hot fixing

--- target/classes/static/clubs.js
+++ target/classes/static/clubs.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
           <div class="card">
     <div class="row no-gutters">
         <div class="col-md-4">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="card-img" alt="Item Photo">
+            <img src="${club.image}" class="card-img" alt="Item Photo">
         </div>
         <div class="col-md-8">
             <div class="card-body">
@@ -51,9 +51,9 @@ $(document).ready(function() {
                 if (index + 2 < recommendedClubs.length) {
                     for (let i = 0; i < 3; i++) {
                         listItem += "<div class=\"card\" style=\"width: 18rem;\">\n" +
-                            "  <img src=\"https://source.unsplash.com/collection/190727/1600x900\" class=\"card-img-top\" alt=\"...\">\n" +
+                            "  <img src=\"" + recommendedClubs[index + i].image + "\"  class=\"card-img-top\" alt=\"...\">\n" +
                             "  <div class=\"card-body\">\n" +
-                            "    <a class=\"card-title\" href=\"club.html?id=" + recommendedClubs[index + i].id + ">" + recommendedClubs[index + i].name + "</a>\n" +
+                            "    <a class=\"card-title\" href=\"club.html?id=" + recommendedClubs[index + i].id + "\">" + recommendedClubs[index + i].name + "</a>\n" +
                             "  </div>\n" +
                             "</div>";
                     }
@@ -61,7 +61,7 @@ $(document).ready(function() {
                 } else {
                     for (let i = index; i < recommendedClubs.length; i++) {
                         listItem += "<div class=\"card\" style=\"width: 18rem;\">\n" +
-                            "  <img src=\"https://source.unsplash.com/collection/190727/1600x900\" class=\"card-img-top\" alt=\"...\">\n" +
+                            "  <img src=\"" + recommendedClubs[index + i].image + "\"  class=\"card-img-top\" alt=\"...\">\n" +
                             "  <div class=\"card-body\">\n" +
                             "    <a class=\"card-title\" href=\"club.html?id=" + recommendedClubs[i].id + ">" + recommendedClubs[i].name + "</a>\n" +
                             "  </div>\n" +
